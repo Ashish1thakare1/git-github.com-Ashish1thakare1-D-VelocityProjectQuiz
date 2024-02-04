faa7671e57077fd4cf23bf127edba856a454c74d
Ashish1thakare1 <158046825+Ashish1thakare1@users.noreply.github.com>
2024-02-04 12:54:18 +0530

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,5 @@
+package com.velocity.groupA.quizBaseApplication;
+
+public interface Admin {
+
+}
---
+++ UserInterface.java
@@ -0,0 +1,14 @@
+package com.velocity.groupA.quizBaseApplication;
+
+public interface UserInterface {
+	public void getStudentReg();
+
+	public void studentLogin();
+
+	public void getListOfQuestions();
+
+	public void getStoreResultIntoDb();
+
+	public void getResult();
+
+}
